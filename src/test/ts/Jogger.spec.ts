03441ea0b91fe40c420b6a77aa8ab48f95f32e50
Wilson Lin <contact@wilsonl.in>
2018-07-22 02:06:33 +1200

    Initial commit

---
+++ src/test/ts/Jogger.spec.ts
@@ -0,0 +1,50 @@
+import * as fs from "fs";
+import {jogTree} from "../../main/ts/Jogger";
+import chai, {expect} from "chai";
+import "mocha";
+import chaiAsPromised = require("chai-as-promised");
+
+chai.use(chaiAsPromised);
+
+const TREE_DIR = __dirname + "/../resources/tree";
+
+const TREE = {
+  a: {
+    aa: {
+      aaa: fs.lstatSync(TREE_DIR + "/a/aa/aaa"),
+      aab: fs.lstatSync(TREE_DIR + "/a/aa/aab"),
+      aac: {
+        aaca: fs.lstatSync(TREE_DIR + "/a/aa/aac/aaca"),
+      },
+    },
+    ab: {
+      aba: fs.lstatSync(TREE_DIR + "/a/ab/aba"),
+      abb: {
+        abba: {
+          abbaa: fs.lstatSync(TREE_DIR + "/a/ab/abb/abba/abbaa"),
+        },
+      },
+    },
+  },
+  b: fs.lstatSync(TREE_DIR + "/b"),
+  c: {
+    ca: fs.lstatSync(TREE_DIR + "/c/ca"),
+    cb: {
+      cba: {
+        cbaa: fs.lstatSync(TREE_DIR + "/c/cb/cba/cbaa"),
+      },
+      cbb: fs.lstatSync(TREE_DIR + "/c/cb/cbb"),
+    },
+  },
+  d: {
+    da: {
+      daa: fs.lstatSync(TREE_DIR + "/d/da/daa"),
+    },
+  },
+};
+
+describe("jogTree", () => {
+  it("should traverse tree recursively and completely", () => {
+    return expect(jogTree({dir: TREE_DIR})).to.eventually.deep.equal(TREE);
+  });
+});
