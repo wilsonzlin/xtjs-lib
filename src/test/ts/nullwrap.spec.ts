58c6f74489c570857040c82a247ae00a5db63cfd
Wilson Lin <contact@wilsonl.in>
2018-07-21 15:44:18 +1200

    Rename to nullwrap

--- src/test/ts/nullwrap.spec.ts
+++ src/test/ts/nullwrap.spec.ts
@@ -1,6 +1,6 @@
 import { expect } from "chai";
 import "mocha";
-import { nullify2 } from "../../main/ts/nullify";
+import { nullwrap } from "../../main/ts/nullwrap";
 
 function fnThatThrowsException(_: string, __: number): string {
   throw new Error();
@@ -22,29 +22,29 @@ function fnThatThrows42_31415(_: string, __: number): string {
   };
 }
 
-describe("nullify", () => {
+describe("nullwrap", () => {
   it("should return a function that returns null instead of throwing an exception", () => {
-    let wrapped = nullify2(fnThatThrowsException);
+    let wrapped = nullwrap(fnThatThrowsException);
     expect(wrapped("", 0)).to.be.null;
   });
 
   it("should return a function that behaves like normal when it doesn't throw an exception", () => {
-    let wrapped = nullify2(fnThatNeverThrowsException);
+    let wrapped = nullwrap(fnThatNeverThrowsException);
     expect(wrapped("", 0)).to.equal("abcdefg");
   });
 
   it("should return a function that returns null when it throws an error object with a string `code` property when a relevant matcher is provided", () => {
-    let wrapped = nullify2(fnThatThrowsENOENT, "ENOENT");
+    let wrapped = nullwrap(fnThatThrowsENOENT, "ENOENT");
     expect(wrapped("", 0)).to.be.null;
   });
 
   it("should return a function that returns null when it throws an error object with a number `code` property when a relevant matcher is provided", () => {
-    let wrapped = nullify2(fnThatThrows42_31415, 42.31415);
+    let wrapped = nullwrap(fnThatThrows42_31415, 42.31415);
     expect(wrapped("", 0)).to.be.null;
   });
 
   it("should return a function that still throws an exception when it doesn't match the provided matcher", () => {
-    let wrapped = nullify2(fnThatThrowsException, "ENOENT");
+    let wrapped = nullwrap(fnThatThrowsException, "ENOENT");
     expect(() => wrapped("", 0)).to.throw();
 
     expect(() => wrapped("", 0)).to.throw();
