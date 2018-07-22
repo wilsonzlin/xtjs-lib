a719db1781e2ad3cee26b8f01f25f3ff84c199be
Wilson Lin <contact@wilsonl.in>
2018-07-23 00:30:59 +1200

    Initial commit

---
+++ src/main/ts/IStats.ts
@@ -0,0 +1,55 @@
+import * as fs from "fs";
+
+export default interface IStats {
+  directory: boolean;
+  file: boolean;
+  blockDevice: boolean;
+  characterDevice: boolean;
+  FIFO: boolean;
+  socket: boolean;
+
+  container: number;
+  inode: number;
+  links: number;
+
+  user: number;
+  group: number;
+  device: number;
+
+  size: number;
+  blocks: number;
+
+  accessed: number;
+  modified: number;
+  changed: number;
+  created: number;
+}
+
+export function convertFromFS(stats: fs.Stats): IStats {
+  let normStats = Object.create(null);
+
+  normStats.directory = stats.isDirectory();
+  normStats.file = stats.isFile();
+  normStats.blockDevice = stats.isBlockDevice();
+  normStats.characterDevice = stats.isCharacterDevice();
+  normStats.FIFO = stats.isFIFO();
+  normStats.socket = stats.isSocket();
+
+  normStats.container = stats.dev;
+  normStats.inode = stats.ino;
+  normStats.links = stats.nlink;
+
+  normStats.user = stats.uid;
+  normStats.group = stats.gid;
+  normStats.device = stats.rdev;
+
+  normStats.size = stats.size;
+  normStats.blocks = stats.blksize;
+
+  normStats.accessed = stats.atimeMs;
+  normStats.modified = stats.mtimeMs;
+  normStats.changed = stats.ctimeMs;
+  normStats.created = stats.birthtimeMs;
+
+  return normStats;
+}
---
+++ src/main/ts/getStats.ts
@@ -0,0 +1,29 @@
+import * as fs from "fs";
+import IStats, { convertFromFS } from "./IStats";
+
+export interface IGetStatsOptions {
+  path: string;
+  resolve?: boolean;
+}
+
+export function getStats({ path, resolve: resolveSymlinks = false }: IGetStatsOptions): Promise<IStats> {
+  return new Promise((resolve, reject) => {
+    fs[resolveSymlinks ? "stat" : "lstat"](path, (err, stats) => {
+      if (err) {
+        return reject(err);
+      }
+
+      let normStats = convertFromFS(stats);
+
+      resolve(normStats);
+    });
+  });
+}
+
+export function getStatsSync({ path, resolve: resolveSymlinks = false }: IGetStatsOptions): IStats {
+  let stats = fs[resolveSymlinks ? "statSync" : "lstatSync"](path);
+
+  let normStats = convertFromFS(stats);
+
+  return normStats;
+}
