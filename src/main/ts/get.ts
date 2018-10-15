43dfd6dd728601038185cdcf0315556e8bc2aea2
Wilson Lin <contact@wilsonl.in>
2018-10-15 14:19:26 +1300

    Create null-safe version of dig function

--- src/main/ts/get.ts
+++ src/main/ts/get.ts
@@ -20,3 +20,29 @@ export function digSafe<T extends Obj<any>, R>(obj: T, path: string): R | undefi
   }
   return val;
 }
+  return val;
+}
+
+export function digSafe<T extends Obj, R>(obj: T, path: string): R | undefined {
+  let components = path.split('.');
+  let val: any = obj;
+  while (components.length) {
+    val = val[components.shift()!];
+    if (val === undefined) {
+      return undefined;
+    }
+  }
+  return val;
+}
+
+export function digNullSafe<T extends Obj, R>(obj: T, path: string): R | null {
+  let components = path.split('.');
+  let val: any = obj;
+  while (components.length) {
+    val = val[components.shift()!];
+    if (val == undefined) {
+      return null;
+    }
+  }
+  return val;
+}
