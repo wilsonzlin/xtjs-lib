fc9a6737ca9a4fdf815dedc13d85bfeb9fdb96d0
Wilson Lin <contact@wilsonl.in>
2018-10-15 14:20:19 +1300

    Conditional property setting functions

--- src/main/ts/set.ts
+++ src/main/ts/set.ts
@@ -1,6 +1,7 @@
 import { pairs } from "./pairs";
 import { Key } from "./keys";
 import { Obj } from "./obj";
+import { hasKey } from "./has";
 
 export function update<T extends Obj, U extends Obj>(obj: T, upd: U): T & U {
   pairs(upd).forEach(([key, value]) => {
@@ -11,4 +12,20 @@ export function update<T extends Obj, U extends Obj>(obj: T, upd: U): T & U {
     }
   });
   return obj as (T & U);
+}
+
+export function setIfExists<T extends Obj>(obj: T, key: Key<T>, value: any): boolean {
+  if (hasKey(obj, key)) {
+    obj[key] = value;
+    return true;
+  }
+  return false;
+}
+
+export function setIfNotExists<T extends Obj>(obj: T, key: Key<T>, value: any): boolean {
+  if (!hasKey(obj, key)) {
+    obj[key] = value;
+    return true;
+  }
+  return false;
 }
