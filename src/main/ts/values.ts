c69ba3e73bb2e60d0fed82cae8ded96e766465c9
Wilson Lin <contact@wilsonl.in>
2018-10-17 13:27:09 +1300

    pluckExcept

--- src/main/ts/values.ts
+++ src/main/ts/values.ts
@@ -7,6 +7,10 @@ export function pluck<T extends Obj, K extends Key<T>> (obj: T, ...keys: K[]): T
   return keys.map(k => obj[k]);
 }
 
+export function pluckExcept<T extends Obj> (obj: T, ...exc: Key<T>[]): T[Key<T>][] {
+  return keys(obj).filter(k => !exc.includes(k)).map(k => obj[k]);
+}
+
 export function values<T extends Obj> (obj: T): Value<T>[] {
   return keys(obj).map(k => obj[k]);
 }
