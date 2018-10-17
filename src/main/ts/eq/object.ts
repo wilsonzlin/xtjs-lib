f1c8c73b66ad51c1c4ec3e4213b829b4dc2a1c3a
Wilson Lin <contact@wilsonl.in>
2018-10-17 13:06:29 +1300

    shallowSubsetObjectEquals

--- src/main/ts/eq/object.ts
+++ src/main/ts/eq/object.ts
@@ -1,6 +1,16 @@
-import { sameKeys } from "../cmp/keys";
+import {sameKeys} from "../cmp/keys";
+import {subsetArray} from "../cmp/array";
 
-export function shallowObjectEquals(a: any, b: any): boolean {
+export function shallowObjectEquals (a: any, b: any): boolean {
   return sameKeys(a, b) &&
     Object.keys(a).every(k => a[k] === b[k]);
+}
+
+export function shallowSubsetObjectEquals (a: any, b: any, keys: any[]): boolean {
+  let A = Object.keys(a);
+  let B = Object.keys(b);
+
+  return subsetArray(keys, A) &&
+    subsetArray(keys, B) &&
+    keys.every(k => a[k] === b[k]);
 }
