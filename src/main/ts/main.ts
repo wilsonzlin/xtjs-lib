95b3406f19a6132c03613a08970a3db0f469eb08
Wilson Lin <contact@wilsonl.in>
2018-10-17 13:06:36 +1300

    Entry point

---
+++ src/main/ts/main.ts
@@ -0,0 +1,7 @@
+export * from "./eq/array";
+export * from "./eq/object";
+export * from "./eq/deep";
+export * from "./cmp/natural";
+export * from "./cmp/array";
+export * from "./cmp/keys";
+export * from "./cmp/native";
