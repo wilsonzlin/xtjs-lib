964278cad6d5b225314c918a6da91ac7f5cbfe8d
Wilson Lin <code@wilsonl.in>
2020-05-29 08:55:23 +1000

    Add classname

---
+++ src/main/ts/dom/classname.ts
@@ -0,0 +1,23 @@
+export type ConditionalClasses = {
+  [className: string]: boolean
+};
+
+export const cls = (...classes: (ConditionalClasses | string | string[] | false | null | undefined)[]) => {
+  const classNames: string[] = [];
+  for (const unit of classes) {
+    if (typeof unit == 'string') {
+      classNames.push(unit);
+    } else if (Array.isArray(unit)) {
+      classNames.push(...unit);
+    } else if (unit === false || unit == undefined) {
+      // Do nothing
+    } else {
+      for (const className of Object.keys(unit)) {
+        if (unit[className]) {
+          classNames.push(className);
+        }
+      }
+    }
+  }
+  return classNames.join(' ');
+};
