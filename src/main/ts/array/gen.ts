337547ac3d6ef1db813dba3af73b9eae914fa061
Wilson Lin <code@wilsonl.in>
2020-06-03 23:41:03 +1000

    Improve blk perf

--- src/main/ts/array/gen.ts
+++ src/main/ts/array/gen.ts
@@ -7,7 +7,7 @@ export const range = (from: number, to?: number, step?: number) => {
 export type ArrayElementProducer<T> = (index: number, array: T[]) => T;
 
 export const blk = <T> (of: number, producer: ArrayElementProducer<T>): T[] => {
-  return Array(of).fill(null).map((_, i, a) => producer(i, a));
+  return Array.from({length: of}, ((_, i: number, a: T[]) => producer(i, a)) as any);
 };
 
 export type SequentialArrayElementProducer<T> = (prev: T, index: number, array: T[]) => T;
