0fd9b0c279c5ea5ab2079369a4bc6747b25cf9c0
Wilson Lin <code@wilsonl.in>
2020-10-03 00:43:49 +1000

    Fix test

--- src/test/ts/array/equals.spec.ts
+++ src/test/ts/array/equals.spec.ts
@@ -1,69 +1,69 @@
-import {stringsArrayEquals} from "array/equals";
-import {secureShuffle} from "array/shuffle";
-import {expect} from "chai";
-import "mocha";
+import {stringArrayEquals} from 'array/equals';
+import {secureShuffle} from 'array/shuffle';
+import {expect} from 'chai';
+import 'mocha';
 
 function shuffle<T> (a: Array<T>): Array<T> {
   return secureShuffle(a.slice());
 }
 
-describe("stringsArrayEquals", () => {
-  it("should return true if both arrays have same strings", () => {
+describe('stringArrayEquals', () => {
+  it('should return true if both arrays have same strings', () => {
     let a = [
-      "5",
-      "",
-      "",
-      "adsf dfag",
-      "  <script> ",
+      '5',
+      '',
+      '',
+      'adsf dfag',
+      '  <script> ',
     ];
     let b = shuffle(a);
-    expect(stringsArrayEquals(a, b)).to.equal(true);
+    expect(stringArrayEquals(a, b)).to.equal(true);
   });
 
-  it("should return false if arrays have different lengths", () => {
+  it('should return false if arrays have different lengths', () => {
     let a = [
-      "5",
-      "",
-      "",
-      "adsf dfag",
-      "  <script> ",
+      '5',
+      '',
+      '',
+      'adsf dfag',
+      '  <script> ',
     ];
     let b = a.slice(1);
-    expect(stringsArrayEquals(a, b)).to.equal(false);
+    expect(stringArrayEquals(a, b)).to.equal(false);
   });
 
-  it("should return false if arrays have different strings", () => {
+  it('should return false if arrays have different strings', () => {
     let a = [
-      "5",
-      "",
-      "",
-      "adsf dfag",
-      "  <script> ",
+      '5',
+      '',
+      '',
+      'adsf dfag',
+      '  <script> ',
     ];
     let b = [
-      "5",
-      "",
-      "",
-      "adsf dfag",
-      " <script> ",
+      '5',
+      '',
+      '',
+      'adsf dfag',
+      ' <script> ',
     ];
-    expect(stringsArrayEquals(a, b)).to.equal(false);
+    expect(stringArrayEquals(a, b)).to.equal(false);
   });
 
-  it("should return false if arrays don't have exactly the same strings and frequencies of them", () => {
+  it('should return false if arrays don\'t have exactly the same strings and frequencies of them', () => {
     let a = [
-      "5",
-      "",
-      "",
-      "adsf dfag",
-      "  <script> ",
+      '5',
+      '',
+      '',
+      'adsf dfag',
+      '  <script> ',
     ];
     let b = [
-      "5",
-      "",
-      "adsf dfag",
-      "  <script> ",
+      '5',
+      '',
+      'adsf dfag',
+      '  <script> ',
     ];
-    expect(stringsArrayEquals(a, b)).to.equal(false);
+    expect(stringArrayEquals(a, b)).to.equal(false);
   });
 });
