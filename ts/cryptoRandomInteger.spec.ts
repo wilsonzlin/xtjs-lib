aa0325b96d9b20071f5f2306b632bddc337cc8fe
Wilson Lin <code@wilsonl.in>
2020-10-23 12:31:35 +1100

    Formatting

--- ts/cryptoRandomInteger.spec.ts
+++ ts/cryptoRandomInteger.spec.ts
@@ -5,9 +5,7 @@ import cryptoRandomInteger from "./cryptoRandomInteger";
 describe("cryptoRandomInteger", () => {
   it("should generate values in the range inclusively", () => {
     for (let i = 0; i < 1e5; i++) {
-      expect(cryptoRandomInteger(-2, 4))
-        .at.least(-2)
-        .at.most(4);
+      expect(cryptoRandomInteger(-2, 4)).at.least(-2).at.most(4);
     }
   });
 
