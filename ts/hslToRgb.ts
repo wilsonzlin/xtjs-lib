3bd9c450d8fc657b6bf62c1472cb4865f4558279
Wilson Lin <code@wilsonl.in>
2020-10-24 12:43:27 +1100

    Add hslToRgb

---
+++ ts/hslToRgb.ts
@@ -0,0 +1,37 @@
+const hueToRgb = (p: number, q: number, t: number) => {
+  if (t < 0) {
+    t += 1;
+  }
+  if (t > 1) {
+    t -= 1;
+  }
+  if (t < 1 / 6) {
+    return p + (q - p) * 6 * t;
+  }
+  if (t < 1 / 2) {
+    return q;
+  }
+  if (t < 2 / 3) {
+    return p + (q - p) * (2 / 3 - t) * 6;
+  }
+  return p;
+};
+
+/**
+ * All parameter values must be between 0 and 1 inclusive.
+ */
+export default (h: number, s: number, l: number): [number, number, number] => {
+  let r, g, b;
+
+  if (s == 0) {
+    r = g = b = l;
+  } else {
+    const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+    const p = 2 * l - q;
+    r = hueToRgb(p, q, h + 1 / 3);
+    g = hueToRgb(p, q, h);
+    b = hueToRgb(p, q, h - 1 / 3);
+  }
+
+  return [r * 255, g * 255, b * 255];
+};
