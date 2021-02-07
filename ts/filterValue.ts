08636a17ef7f35fba5d676163729ef2b9589602c
Wilson Lin <code@wilsonl.in>
2021-02-07 13:15:03 +1100

    Support type predicates with filterValue

--- ts/filterValue.ts
+++ ts/filterValue.ts
@@ -1,2 +1,11 @@
-export default <V>(value: V, cond: (value: V) => any) =>
-  cond(value) ? value : undefined;
+export default function <V>(
+  value: unknown,
+  cond: (value: unknown) => value is V
+): V | undefined;
+export default function <V>(
+  value: V,
+  cond: (value: V) => boolean
+): V | undefined;
+export default function <V>(value: V, cond: (value: V) => any): V | undefined {
+  return cond(value) ? value : undefined;
+}
