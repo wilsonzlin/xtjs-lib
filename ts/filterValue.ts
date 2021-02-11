3f781c8e211c6aba32146fa71b24494efc22989b
Wilson Lin <code@wilsonl.in>
2021-02-11 23:48:08 +1100

    Remove faulty filterValue sig

--- ts/filterValue.ts
+++ ts/filterValue.ts
@@ -1,11 +1,4 @@
-export default function <V>(
-  value: unknown,
-  cond: (value: unknown) => value is V
-): V | undefined;
-export default function <V>(
-  value: V,
-  cond: (value: V) => boolean
-): V | undefined;
-export default function <V>(value: V, cond: (value: V) => any): V | undefined {
-  return cond(value) ? value : undefined;
-}
+// We cannot have an overload like `<V>(value: unknown, cond: (v: unknown) => v is V): V | undefined`
+// as it would always override this one (and vice versa).
+export default <V>(value: V, cond: (value: V) => any): V | undefined =>
+  cond(value) ? value : undefined;
