0416064be06136e1ba0ca5d65e12818f887e91e6
Wilson Lin <code@wilsonl.in>
2021-02-20 12:20:21 +1100

    assertMatches

---
+++ ts/assertMatches.ts
@@ -0,0 +1,11 @@
+import AssertionError from "./AssertionError";
+
+export default (
+  val: string,
+  regex: RegExp,
+  message: string = `String does not match regex ${regex}`
+) => {
+  if (!regex.test(val)) {
+    throw new AssertionError(message);
+  }
+};
