b0e331c4f9525bcb21bf031b525867060d84cdcb
Wilson Lin <code@wilsonl.in>
2021-02-24 18:43:08 +1100

    Fix isEmpty

--- ts/isEmpty.ts
+++ ts/isEmpty.ts
@@ -5,10 +5,10 @@ export default <C extends { length: number } | { size: number }>(
 ): boolean => {
   // Do not use 'in' operator on string, it will throw an error.
   if (typeof collection == "string") {
-    return !!collection;
+    return !collection;
   }
   if (typeof collection == "object" && collection) {
-    return !!("size" in collection ? collection["size"] : collection["length"]);
+    return !("size" in collection ? collection["size"] : collection["length"]);
   }
   throw new UnreachableError();
 };
