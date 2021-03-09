acd203ddf29b7c4778d8b1c245b1d9924e3ad6e0
Wilson Lin <code@wilsonl.in>
2021-03-09 14:41:01 +1100

    Verify base64 in decodePem

--- ts/decodePem.ts
+++ ts/decodePem.ts
@@ -12,5 +12,8 @@ export default (raw: string) => {
   const base64Data = raw
     .slice(headerMatch[0].length, footerMatch.index)
     .replace(/\s+/g, "");
+  if (!/^[a-zA-Z0-9/+=]+$/.test(base64Data)) {
+    throw new Error("PEM data has non-base64 characters");
+  }
   return Buffer.from(base64Data, "base64");
 };
