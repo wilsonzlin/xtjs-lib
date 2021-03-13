7d505fdbef80ee246e3bfa382c6f113730ade357
Wilson Lin <code@wilsonl.in>
2021-03-14 02:43:33 +1100

    Default to nativeOrdering

--- ts/derivedComparator.ts
+++ ts/derivedComparator.ts
@@ -1,6 +1,7 @@
 import Comparator from "./Comparator";
+import nativeOrdering from "./nativeOrdering";
 
 export default <O, D>(
   derivation: (val: O) => D,
-  derivedComparator: Comparator<D>
+  derivedComparator: Comparator<D> = nativeOrdering as any
 ): Comparator<O> => (a, b) => derivedComparator(derivation(a), derivation(b));
