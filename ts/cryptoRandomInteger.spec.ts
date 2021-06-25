fc4864ef3b0495bc90cfd8b36529980b83b2a382
Wilson Lin <code@wilsonl.in>
2021-06-25 21:27:38 +1000

    Remove arbitrary CRNG randomness tests

--- ts/cryptoRandomInteger.spec.ts
+++ ts/cryptoRandomInteger.spec.ts
@@ -8,47 +8,4 @@ describe("cryptoRandomInteger", () => {
       expect(cryptoRandomInteger(-2, 4)).at.least(-2).at.most(4);
     }
   }).timeout(60000);
-
-  it("should be reasonably random", () => {
-    const min = -2;
-    const max = 4;
-    const amount = 1e6;
-
-    const threshold = 1000;
-
-    const slots: { [slot: number]: number } = Object.create(null);
-
-    for (let slot = min; slot <= max; slot++) {
-      slots[slot] = 0;
-    }
-
-    for (let i = 0; i < amount; i++) {
-      const rnd = cryptoRandomInteger(min, max);
-      if (!Number.isSafeInteger(rnd) || rnd < min || rnd > max) {
-        throw new RangeError(`RNG generated out-of-range value: ${rnd}`);
-      }
-      slots[rnd]++;
-    }
-
-    let minCount = +Infinity;
-    let minCountSlot;
-    let maxCount = -Infinity;
-    let maxCountSlot;
-
-    for (let slot = min; slot <= max; slot++) {
-      const count = slots[slot];
-
-      if (count < minCount) {
-        minCount = count;
-        minCountSlot = slot;
-      }
-
-      if (count > maxCount) {
-        maxCount = count;
-        maxCountSlot = slot;
-      }
-    }
-
-    expect(Math.abs(maxCount - minCount)).lessThan(threshold);
-  }).timeout(60000);
 });
--- ts/cryptoRandomPortion.spec.ts
+++ ts/cryptoRandomPortion.spec.ts
@@ -8,42 +8,4 @@ describe("cryptoRandomPortion", () => {
       expect(cryptoRandomPortion()).at.least(0).lessThan(1);
     }
   }).timeout(60000);
-
-  it("should be reasonably random", () => {
-    const slotsCount = 20;
-    const amount = 1e6;
-
-    const threshold = 1000;
-
-    const slots = Array(slotsCount).fill(0);
-
-    for (let i = 0; i < amount; i++) {
-      const rnd = cryptoRandomPortion();
-      if (rnd < 0 || rnd >= 1 || !Number.isFinite(rnd)) {
-        throw new RangeError(`RNG generated value outside range: ${rnd}`);
-      }
-      slots[Math.floor(rnd * slotsCount)]++;
-    }
-
-    let minCount = +Infinity;
-    let minCountSlot;
-    let maxCount = -Infinity;
-    let maxCountSlot;
-
-    for (const slot in slots) {
-      const count = slots[slot];
-
-      if (count < minCount) {
-        minCount = count;
-        minCountSlot = slot;
-      }
-
-      if (count > maxCount) {
-        maxCount = count;
-        maxCountSlot = slot;
-      }
-    }
-
-    expect(Math.abs(maxCount - minCount)).lessThan(threshold);
-  }).timeout(60000);
 });
