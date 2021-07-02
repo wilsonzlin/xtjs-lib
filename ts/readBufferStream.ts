8a19edbdc174d0dedd27d00ff999102c16c5df5e
Wilson Lin <code@wilsonl.in>
2021-07-02 22:26:27 +1000

    Update type

--- ts/readBufferStream.ts
+++ ts/readBufferStream.ts
@@ -9,6 +9,4 @@ export default async (
   // see https://nodejs.org/api/buffer.html#buffer_buffers_and_typedarrays.
   stream: IsomorphicReadableStream<Uint8Array | ArrayBufferLike>
 ) =>
-  concatBuffers(
-    ...(await arrayFromAsyncIterable(isomorphicReadableStream(stream)))
-  );
+  concatBuffers(await arrayFromAsyncIterable(isomorphicReadableStream(stream)));
