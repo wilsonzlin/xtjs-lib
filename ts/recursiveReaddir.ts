577738e55ca8a551bf3a07ca3646443339c3148e
Wilson Lin <code@wilsonl.in>
2021-07-03 21:06:34 +1000

    [recursiveReaddir] Ignore race condition errors by default

--- ts/recursiveReaddir.ts
+++ ts/recursiveReaddir.ts
@@ -1,16 +1,33 @@
 import { join, sep } from "path";
 import { readdir } from "fs/promises";
 
+const DEFAULT_IGNORED_ERRORS = ["EACCES", "ENOENT", "ENOTDIR"];
+
 export default async function* recursiveReaddir(
   dir: string,
   filter?: RegExp,
-  prefix: string = ""
+  prefix: string = "",
+  options?: {
+    ignoredReaddirErrors?: string[];
+  }
 ): AsyncGenerator<string, void, void> {
-  for (const ent of await readdir(dir, { withFileTypes: true })) {
+  let ents;
+  try {
+    ents = await readdir(dir, { withFileTypes: true });
+  } catch (e) {
+    if (
+      (options?.ignoredReaddirErrors ?? DEFAULT_IGNORED_ERRORS).includes(e.code)
+    ) {
+      return;
+    }
+    throw e;
+  }
+
+  for (const ent of ents) {
     const path = join(dir, ent.name);
     if (!filter || filter.test(path)) {
       if (ent.isDirectory()) {
-        yield* recursiveReaddir(path, filter, prefix + ent.name + sep);
+        yield* recursiveReaddir(path, filter, prefix + ent.name + sep, options);
       } else {
         yield prefix + ent.name;
       }
