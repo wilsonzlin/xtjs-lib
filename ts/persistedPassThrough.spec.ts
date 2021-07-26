0830b1f2a887de18fe80560a84e4771f200cefa2
Wilson Lin <code@wilsonl.in>
2021-07-26 17:17:49 +1000

    persistedPassThrough

---
+++ ts/persistedPassThrough.spec.ts
@@ -0,0 +1,53 @@
+import { expect } from "chai";
+import { PassThrough } from "stream";
+import asyncTimeout from "./asyncTimeout";
+import cryptoRandomHex from "./cryptoRandomHex";
+import numberGenerator from "./numberGenerator";
+import persistedPassThrough from "./persistedPassThrough";
+import readBufferStream from "./readBufferStream";
+
+const bytes = (...bytes: number[]) => Buffer.from(bytes);
+
+describe("persistedPassThrough", () => {
+  it("should pass correct data through", async () => {
+    const END = 10;
+    const up = new PassThrough();
+    const path = `/tmp/extlib-persistedpassthrough-test-${cryptoRandomHex(16)}`;
+    const pt = await persistedPassThrough(up, path);
+    const downs = Promise.all(
+      ([
+        [0],
+        [100, 7],
+        [200, 5, 10],
+        [200, 0, 20],
+        [500, 1, 8],
+        [400, undefined, undefined],
+        [300, 3],
+        [150, 6],
+        [900, 10],
+        [1100, 11],
+      ] as const).map(async ([delay, start, end]) => {
+        await asyncTimeout(delay);
+        const d = pt.downstream(start, end);
+        expect([...(await readBufferStream(d))]).to.deep.equal([
+          ...numberGenerator(start ?? 0, Math.min(end ?? Infinity, END) + 1),
+        ]);
+      })
+    );
+    up.write(bytes(0));
+    await asyncTimeout(100);
+    up.write(bytes(1, 2));
+    await asyncTimeout(200);
+    up.write(bytes(3));
+    await asyncTimeout(100);
+    up.write(bytes(4, 5, 6));
+    await asyncTimeout(300);
+    up.write(bytes(7));
+    await asyncTimeout(100);
+    up.write(bytes(8, 9));
+    await asyncTimeout(200);
+    up.write(bytes(10));
+    up.end();
+    await downs;
+  }).timeout(60000);
+});
---
+++ ts/persistedPassThrough.ts
@@ -0,0 +1,109 @@
+import { createReadStream } from "fs";
+import { open } from "fs/promises";
+import { PassThrough, Readable, Writable } from "stream";
+import assertState from "./assertState";
+import rangeIntersection from "./rangeIntersection";
+
+export default async (upstream: Readable, persistencePath: string) => {
+  const downstreams: {
+    stream: Writable;
+    // Offset of the next byte yet to be received.
+    next: number;
+    // Inclusive.
+    end: number;
+    initialised: Promise<any>;
+  }[] = [];
+  // Offset of the next byte yet to be received.
+  // This means we don't have byte at `next`, or any byte after it.
+  let next = 0;
+  let ended = false;
+
+  // Use file handle instead of writable stream to ensure writes are flushed each time.
+  const persistence = await open(persistencePath, "w");
+  (async () => {
+    // Use async for loop to ensure chunks are processed one-by-one in order.
+    for await (const chunk of upstream) {
+      if (!Buffer.isBuffer(chunk)) {
+        throw new TypeError(`Received non-Buffer data`);
+      }
+      const chunkStart = next;
+      const chunkEnd = chunkStart + chunk.byteLength - 1;
+      await persistence.write(chunk);
+      // `next` must be updated AFTER writing has been flushed to avoid race condition where a downstream is initialised based on a larger value of `next` but bytes haven't actually been written yet.
+      next += chunk.byteLength;
+      for (const d of downstreams) {
+        if (d.next <= d.end) {
+          // Downstream still has pending bytes.
+          const range = rangeIntersection(
+            [chunkStart, chunkEnd],
+            [d.next, d.end]
+          );
+          if (range) {
+            d.initialised.then(() =>
+              d.stream.write(
+                chunk.slice(range[0] - chunkStart, range[1] - chunkStart + 1)
+              )
+            );
+            d.next = range[1] + 1;
+          }
+        } else {
+          d.initialised.then(() => d.stream.end());
+        }
+      }
+    }
+    ended = true;
+    for (const d of downstreams) {
+      d.initialised.then(() => d.stream.end());
+    }
+  })().finally(() => persistence.close());
+
+  return {
+    downstream(start = 0, end = Infinity) {
+      if (ended) {
+        return createReadStream(persistencePath, { start, end });
+      }
+      // Cache `next` value.
+      // We don't need to cache `writing` as code is synchronous up to usage of it.
+      const persistedLen = next;
+      assertState(start <= end);
+      const stream = new PassThrough();
+      const downstreamNext = Math.max(start, persistedLen);
+      downstreams.push({
+        stream,
+        end,
+        next: downstreamNext,
+        // We need to add this downstream immediately to `downstreams` as `next` could change if we await an asynchronous read from the file system.
+        initialised: (async () => {
+          if (start >= persistedLen) {
+            return;
+          }
+          const fh = await open(persistencePath, "r");
+          try {
+            const length = Math.min(end + 1, persistedLen) - start;
+            assertState(length > 0);
+            const buffer = Buffer.alloc(length);
+            const { bytesRead } = await fh.read({
+              buffer,
+              position: start,
+              length,
+            });
+            assertState(
+              bytesRead === length,
+              `Expected to read ${length} bytes from persisted file, but got ${bytesRead}`
+            );
+            stream.write(buffer);
+          } finally {
+            await fh.close();
+          }
+        })().catch((err) =>
+          stream.destroy(
+            new Error(
+              `Encountered ${err.name} while initialising downstream: ${err.message}`
+            )
+          )
+        ),
+      });
+      return stream;
+    },
+  };
+};
