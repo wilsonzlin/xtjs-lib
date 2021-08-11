1df5b74e61c48b2bc9f848c6a64f4ea316d6a5d3
Wilson Lin <code@wilsonl.in>
2021-08-11 17:57:45 +1000

    Fix findMap type checking

--- ts/findMap.ts
+++ ts/findMap.ts
@@ -1,10 +1,10 @@
 export default <V, R>(
   it: Iterable<V>,
-  mapper: (val: V) => R
+  mapper: (val: V) => R | undefined
 ): R | undefined => {
   for (const val of it) {
     const mapped = mapper(val);
-    if (mapped) {
+    if (mapped !== undefined) {
       return mapped;
     }
   }
