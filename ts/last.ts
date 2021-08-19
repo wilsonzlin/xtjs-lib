75f22d52758c790876e16b6c4ef4879b2bcee3ec
Wilson Lin <code@wilsonl.in>
2021-08-20 00:05:38 +1000

    Minor comment

--- ts/last.ts
+++ ts/last.ts
@@ -1,1 +1,3 @@
+// The return type isn't optional as the value at an index isn't optional in TS.
+// However, in both cases the value is undefined if the array is empty.
 export default <T>(seq: ArrayLike<T>): T => seq[seq.length - 1];
