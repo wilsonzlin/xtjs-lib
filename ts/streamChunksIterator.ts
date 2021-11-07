70698ba46328467b3f2df6f481644f1f0be99a39
Wilson Lin <code@wilsonl.in>
2021-11-07 13:14:37 +1100

    Add comment

--- ts/streamChunksIterator.ts
+++ ts/streamChunksIterator.ts
@@ -3,6 +3,7 @@ import Chunker from "./chunker";
 export default async function* (
   input: AsyncIterable<Uint8Array>,
   size: number,
+  // This is default false by assumption that most people will want to keep all data and not just silently throw away a chunk. Even if they require all exact-size chunks, they should still receive all chunks and make an assertion on each chunk.
   discardRemaining = false
 ) {
   const chunker = new Chunker(size);
