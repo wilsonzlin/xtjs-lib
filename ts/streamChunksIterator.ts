1f7659bf9569f8cf16d96dbdc22e75e0279fd2c8
Wilson Lin <code@wilsonl.in>
2021-11-07 13:12:18 +1100

    [streamChunksIterator] Yield remaining data unless explicitly disallowed

--- ts/streamChunksIterator.ts
+++ ts/streamChunksIterator.ts
@@ -2,11 +2,15 @@ import Chunker from "./chunker";
 
 export default async function* (
   input: AsyncIterable<Uint8Array>,
-  size: number
+  size: number,
+  discardRemaining = false
 ) {
   const chunker = new Chunker(size);
   for await (const chunk of input) {
     yield* chunker.push(chunk);
   }
-  return chunker.takeRemaining();
+  const rem = chunker.takeRemaining();
+  if (!discardRemaining && rem.byteLength) {
+    yield rem;
+  }
 }
