e99164f6a86ecba89cdc858000ed94246ba38d66
Wilson Lin <code@wilsonl.in>
2021-11-15 19:27:45 +1100

    [toggleArrayValue] Autodetermine action if toggle parameter is not provided

--- ts/toggleArrayValue.ts
+++ ts/toggleArrayValue.ts
@@ -1,7 +1,7 @@
 // Always returns a new array.
-export default <T>(ary: ReadonlyArray<T>, val: T, toggle: boolean): T[] => {
+export default <T>(ary: ReadonlyArray<T>, val: T, toggle?: boolean): T[] => {
   const res = ary.filter((e) => e !== val);
-  if (toggle) {
+  if ((toggle !== undefined && toggle) || (toggle === undefined && res.length === ary.length)) {
     res.push(val);
   }
   return res;
