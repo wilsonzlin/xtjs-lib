f5ff5ea42324140d2fa038a9ac7ac1578f7b6f32
Wilson Lin <code@wilsonl.in>
2021-11-15 19:29:05 +1100

    Formatting

--- ts/toggleArrayValue.ts
+++ ts/toggleArrayValue.ts
@@ -1,7 +1,10 @@
 // Always returns a new array.
 export default <T>(ary: ReadonlyArray<T>, val: T, toggle?: boolean): T[] => {
   const res = ary.filter((e) => e !== val);
-  if ((toggle !== undefined && toggle) || (toggle === undefined && res.length === ary.length)) {
+  if (
+    (toggle !== undefined && toggle) ||
+    (toggle === undefined && res.length === ary.length)
+  ) {
     res.push(val);
   }
   return res;
