76239771b075e5055e297aa169cfa9d445c156a1
Wilson Lin <code@wilsonl.in>
2021-11-28 18:29:09 +1100

    Improve assertInstanceOf error message

--- ts/assertInstanceOf.ts
+++ ts/assertInstanceOf.ts
@@ -1,9 +1,12 @@
 import AssertionError from "./AssertionError";
+import describeType from "./describeType";
 
 export default <V>(
   val: unknown,
   type: new (...args: any[]) => V,
-  msg: string = "Unexpected type of instance"
+  msg: string = `Unexpected type of instance (expected ${
+    type.name
+  }, got ${describeType(val)})`
 ): V => {
   if (!(val instanceof type)) {
     throw new AssertionError(msg);
