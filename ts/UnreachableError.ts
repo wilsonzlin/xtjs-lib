2b384cda065e2b6e68e7dacd33af33937ae83592
Wilson Lin <code@wilsonl.in>
2022-04-20 13:20:50 +1000

    [UnreachableError] Allow any value

--- ts/UnreachableError.ts
+++ ts/UnreachableError.ts
@@ -2,7 +2,7 @@ import AssertionError from "./AssertionError";
 import mapDefined from "./mapDefined";
 
 export default class UnreachableError extends AssertionError {
-  constructor(val?: never, msg?: string) {
+  constructor(val?: any, msg?: string) {
     super(msg ?? mapDefined(val, String));
   }
 }
