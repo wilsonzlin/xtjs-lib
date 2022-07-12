54248e07811e0418d3ad83bedac861ef80a50438
Wilson Lin <code@wilsonl.in>
2022-07-12 19:02:58 +1000

    Do not take fileSize for parseRangeHeader

--- ts/parseRangeHeader.ts
+++ ts/parseRangeHeader.ts
@@ -1,6 +1,6 @@
 import mapExists from "./mapExists";
 
-export default (rangeHeaderValue: string, fileSize: number) => {
+export default (rangeHeaderValue: string) => {
   const rangeParts = /^bytes=(0|[1-9][0-9]*)-(0|[1-9][0-9]*)?$/.exec(
     rangeHeaderValue
   );
@@ -8,15 +8,12 @@ export default (rangeHeaderValue: string, fileSize: number) => {
     return undefined;
   }
   const start = Number.parseInt(rangeParts[1], 10);
-  const end =
-    mapExists(rangeParts[2], (p) => Number.parseInt(p, 10)) ?? fileSize - 1;
+  const end = mapExists(rangeParts[2], (p) => Number.parseInt(p, 10));
   if (
     !Number.isSafeInteger(start) ||
     !Number.isSafeInteger(end) ||
     start < 0 ||
-    end < 0 ||
-    start > end ||
-    end >= fileSize
+    (end != undefined && (end < 0 || start > end))
   ) {
     return undefined;
   }
