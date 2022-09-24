bf0a19637384d7010b364cff97b3980b6294146d
Wilson Lin <code@wilsonl.in>
2022-09-25 00:55:16 +1000

    Implement ManagedAbortableChain

--- ts/abortableChain.ts
+++ ts/abortableChain.ts
@@ -6,14 +6,18 @@ type AbortableChainStageExecutor<I, O> = (
   signal: AbortSignal
 ) => O | Promise<O>;
 
-type AbortableChainStage<I> = {
+type AbortableChainStage<I, ExecAddProps> = {
   add<O>(
     fn: AbortableChainStageExecutor<I, O>
-  ): AbortableChainStage<Awaited<O>>;
-  exec(): Promise<I>;
+  ): AbortableChainStage<Awaited<O>, ExecAddProps>;
+  exec(): Promise<I> & ExecAddProps;
 };
 
-export default class AbortableChain implements AbortableChainStage<void> {
+type ManagedAbortableChainStageExecAddProps = {
+  abort: () => void;
+};
+
+export default class AbortableChain implements AbortableChainStage<void, {}> {
   private readonly stages = Array<AbortableChainStageExecutor<any, any>>();
 
   constructor(
@@ -23,7 +27,7 @@ export default class AbortableChain implements AbortableChainStage<void> {
 
   add<O>(
     fn: (lastStageOutput: any, signal: AbortSignal) => O | Promise<O>
-  ): AbortableChainStage<Awaited<O>> {
+  ): AbortableChainStage<Awaited<O>, {}> {
     this.stages.push(fn);
     return this as any;
   }
@@ -35,5 +39,34 @@ export default class AbortableChain implements AbortableChainStage<void> {
       out = await stage(out, this.abortSignal);
     }
     return out;
+  }
+}
+
+export class ManagedAbortableChain extends AbortableChain {
+  private readonly controller: AbortController;
+
+  constructor() {
+    const controller = new AbortController();
+    super(controller.signal);
+    this.controller = controller;
+  }
+
+  override add<O>(
+    fn: (lastStageOutput: any, signal: AbortSignal) => O | Promise<O>
+  ): AbortableChainStage<Awaited<O>, ManagedAbortableChainStageExecAddProps> {
+    return super.add(fn) as any;
+  }
+
+  override async exec() {
+    const promise = super.exec();
+    // Don't return Promise as custom properties set on it like `abort` won't stick.
+    const res: Promise<any> & ManagedAbortableChainStageExecAddProps = {
+      [Symbol.toStringTag]: ManagedAbortableChain.name,
+      then: (f, r) => promise.then(f, r),
+      catch: (r) => promise.catch(r),
+      finally: (n) => promise.finally(n),
+      abort: () => this.controller.abort(),
+    };
+    return res;
   }
 }
