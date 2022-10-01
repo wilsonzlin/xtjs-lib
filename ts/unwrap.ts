2684190009b57ccf0b90a17dcf2fe5f4d1b3b29e
Wilson Lin <code@wilsonl.in>
2022-10-02 10:37:36 +1100

    Make unwrap arg readonly

--- ts/unwrap.ts
+++ ts/unwrap.ts
@@ -1,7 +1,7 @@
 import assertState from "./assertState";
 
 export default <T>(
-  arr: T[],
+  arr: readonly T[],
   msg = `Expected array of exactly one element (got ${arr.length})`
 ): T => {
   assertState(arr.length == 1, msg);
