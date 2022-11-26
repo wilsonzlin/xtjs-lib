453c01bd79f8dce281c6ce2028d7e69c882ddba6
Wilson Lin <code@wilsonl.in>
2022-11-26 22:28:00 +1100

    take: up to n

--- ts/take.ts
+++ ts/take.ts
@@ -1,5 +1,9 @@
 export default function* <V>(it: Iterator<V>, n: number): Generator<V> {
   for (let i = 0; i < n; i++) {
-    yield it.next().value;
+    const e = it.next();
+    if (e.done) {
+      return;
+    }
+    yield e.value;
   }
 }
