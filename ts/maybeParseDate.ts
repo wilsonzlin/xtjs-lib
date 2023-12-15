14ea3d4fb2cb5079ba91aec528832078a88a8334
Wilson Lin <code@wilsonl.in>
2023-12-15 12:42:11 +1100

    Reformatting

--- ts/maybeParseDate.ts
+++ ts/maybeParseDate.ts
@@ -1,3 +1,3 @@
 import filterValue from "./filterValue";
 
-export default (v: any) => filterValue(new Date(v), v => !isNaN(v.getTime()));
+export default (v: any) => filterValue(new Date(v), (v) => !isNaN(v.getTime()));
