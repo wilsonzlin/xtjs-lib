ccdbea901b6eaf8d39229693aeaa1f8a72578658
Wilson Lin <code@wilsonl.in>
2024-04-07 00:30:01 +1100

    Batcher

---
+++ ts/Batcher.spec.ts
@@ -0,0 +1,27 @@
+import { expect } from "chai";
+import asyncTimeout from "./asyncTimeout";
+import { Batcher } from "./Batcher";
+
+describe("Batcher", () => {
+  it("should batch", async () => {
+    const ops = Array<string | number>();
+    const batcher = new Batcher(async (vals) => {
+      await asyncTimeout(100);
+      return vals;
+    });
+    batcher.execute(0).then(() => ops.push(0));
+    batcher.execute(1).then(() => ops.push(1));
+    batcher.execute(2).then(() => ops.push(2));
+    await asyncTimeout(100);
+    ops.push("cp0");
+    await asyncTimeout(500);
+    ops.push("cp1");
+    batcher.execute(3).then(() => ops.push(3));
+    batcher.execute(4).then(() => ops.push(4));
+    batcher.execute(5).then(() => ops.push(5));
+    ops.push("cp2");
+    await asyncTimeout(500);
+    ops.push("cp3");
+    expect(ops).to.deep.equal([0, "cp0", 1, 2, "cp1", "cp2", 3, 4, 5, "cp3"]);
+  });
+});
---
+++ ts/Batcher.ts
@@ -0,0 +1,36 @@
+import assertState from "./assertState";
+
+export class Batcher<T, R> {
+  private readonly q = Array<{
+    resolve: (r: R) => void;
+    input: T;
+  }>();
+  private flushing = false;
+
+  constructor(private readonly fn: (vals: T[]) => Promise<R[]>) {}
+
+  private async maybeFlush() {
+    // We don't need to batch by timers (e.g. debounce), as if there is a lot of activity, it will be efficient because the subsequent batches will be optimally sized, and if there isn't, then it doesn't matter. OTOH, using a timer is far more complex and subtle.
+    if (this.flushing) {
+      return;
+    }
+    this.flushing = true;
+    while (this.q.length) {
+      const dq = this.q.splice(0);
+      const outputs = await this.fn(dq.map((e) => e.input));
+      assertState(outputs.length === dq.length);
+      for (const [i, out] of outputs.entries()) {
+        dq[i].resolve(out);
+      }
+    }
+    this.flushing = false;
+  }
+
+  public async execute(input: T) {
+    return new Promise<R>((resolve) => {
+      this.q.push({ resolve, input });
+      // Intentionally do not wait for Promise. The function is async just so we can use `await` in it.
+      void this.maybeFlush();
+    });
+  }
+}
